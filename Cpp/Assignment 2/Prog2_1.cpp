8714eb4af5cebc0fcdf8fe032349981023de299e
Dmitrii Kondratev <dimonloboto@gmail.com>
2013-01-29 00:30:27 -0600

    Formated source, added comments

--- Cpp/Assignment 2/Prog2_1.cpp
+++ Cpp/Assignment 2/Prog2_1.cpp
@@ -1,12 +1,13 @@
-//Assignment 2, program 1
 //Dmitrii Kondratev
-//
+//COSC-1337
+//Programming Assignment 2, problem 1
 #include <iostream>
 #include <string>
 using namespace std;
 
 
 const int EMPLOYEESNUMBER = 5;
+//number of employees used in all loops for easier modifications
 
 struct Employee
 {
@@ -16,6 +17,7 @@ struct Employee
 	float PayRate;
 };
 
+//Employee array declaration
 Employee EmplArr[EMPLOYEESNUMBER];
 
 void initEmployeesAll();
@@ -26,23 +28,28 @@ bool var2 = (var1=='y');
 int var3;
 
 void startSearch(int);
+
 void searchByID();
 void searchByLastName();
 void searchByHourlyPay();
+//those three functions are starting the searach and choice between three functions comes form user inputed choice
+
 
 void outputResult(int, int);
 
 int main()
 {
 	initEmployeesAll();
+	//assignment of all employees in array in function initEmployeesAll();
 	while (var2)
 	{
-          cout << "Choose your search method:" << endl << "(1) - Search by ID" << endl << "(2) - Search by last name" << endl << "(3) - Search by hourly pay" << endl;
-          cin >> var3;
-          startSearch(var3);
-          cout << "Do you want to search for another employee? (y/n)" << endl;
-          cin >> var1;
-          var2 = (var1=='y') || (var1=='Y');
+        cout << "Choose your search method:" << endl << "(1) - Search by ID" << endl << "(2) - Search by last name" << endl << "(3) - Search by hourly pay" << endl;
+        cin >> var3;
+        startSearch(var3);
+        cout << "Do you want to search for another employee? (y/n)" << endl;
+        cin >> var1;
+        var2 = (var1=='y') || (var1=='Y');
+		//loops for multiple searches in one program run
     }
     system("PAUSE");
 	return 0;
@@ -50,6 +57,11 @@ int main()
 
 void initEmployee(int par1, int par2, string par3, string par4, float par5)
 {
+//par1 - number of employee in array + 1, so they start with 1
+//par2 - Employee ID
+//par3 - First Name
+//par4 - Last Name
+//par5 - hourly pay rate
 	EmplArr[par1-1].ID = par2;
 	EmplArr[par1-1].FirstName = par3;
 	EmplArr[par1-1].LastName = par4;
@@ -58,97 +70,104 @@ void initEmployee(int par1, int par2, string par3, string par4, float par5)
 
 void initEmployeesAll()
 {
-     initEmployee(1, 216, "William", "Fullerton", 13.50);
-     initEmployee(2, 150, "Ashley", "Ross", 14.00);
-     initEmployee(3, 78, "Sharon", "Webb", 17.20);
-     initEmployee(4, 180, "George", "Ross", 16.80);
-     initEmployee(5, 10, "Eli", "Manning", 35.00);
-     
+	//when more employees needed just add lines here
+	//initEmployee(number, ID, "FirstName", "LastName", payRate);
+    initEmployee(1, 216, "William", "Fullerton", 13.50);
+    initEmployee(2, 150, "Ashley", "Ross", 14.00);
+    initEmployee(3, 78, "Sharon", "Webb", 17.20);
+    initEmployee(4, 180, "George", "Ross", 16.80);
+    initEmployee(5, 10, "Eli", "Manning", 35.00);
+    
 }
 
 void startSearch(int par1)
 {
-     switch (par1)
-     {
-     case 1:          
-          searchByID();
-          break;
-     case 2:          
-          searchByLastName();
-          break;
-     case 3:
-          searchByHourlyPay();
-          break;
-     default:
-             
-             cout << "Please choose one of the option above!" << endl;
-          break;
-          }
+//choose search method, and start different functions based of user input
+	switch (par1)
+    {
+    case 1:          
+        searchByID();
+        break;
+    case 2:          
+        searchByLastName();
+        break;
+    case 3:
+        searchByHourlyPay();
+        break;
+    default:
+        //Handler for invalid input data
+        cout << "Please choose one of the option above!" << endl;
+        break;
+        }
 }
 void searchByID()
 {
-     int Id;
-     bool emptyResult = true;
-     int extraCounter = 1;
-     cout << "Please enter employee ID: ";
-     cin >> Id;
-     cout << "Employees found:" << endl;
-     for (int i = 0; i < EMPLOYEESNUMBER; i++)
-     {
-         if (EmplArr[i].ID == Id)
-         {
-              outputResult(extraCounter, i);
-              emptyResult = false;
-         }         
-     }
-     if (emptyResult)
-     {cout << "No employees matching criteria found" << endl;}
+    int Id;
+    bool emptyResult = true;
+    int extraCounter = 1;
+    cout << "Please enter employee ID: ";
+    cin >> Id;
+    cout << "Employees found:" << endl;
+    for (int i = 0; i < EMPLOYEESNUMBER; i++)
+    {
+        if (EmplArr[i].ID == Id)
+        {
+            outputResult(extraCounter, i);
+            emptyResult = false;
+        }         
+    }
+    if (emptyResult)
+    {cout << "No employees matching criteria found" << endl;}
      
 }
 
 void searchByLastName()
 {
-     string nam;
-     bool emptyResult = true;
-     int extraCounter = 1;
-     cout << "Please enter employee Last Name: ";
-     cin >> nam;
-     cout << "Employees found:" << endl;
-     for (int i = 0; i < EMPLOYEESNUMBER; i++)
-     {
-         if (EmplArr[i].LastName == nam)
-         {
-              outputResult(extraCounter, i);
-              emptyResult = false;
-         }         
-     }
-     if (emptyResult)
-     {cout << "No employees matching criteria found" << endl;}
+    string nam;
+    bool emptyResult = true;
+    int extraCounter = 1;
+    cout << "Please enter employee Last Name: ";
+    cin >> nam;
+    cout << "Employees found:" << endl;
+    for (int i = 0; i < EMPLOYEESNUMBER; i++)
+    {
+        if (EmplArr[i].LastName == nam)
+        {
+            outputResult(extraCounter, i);
+            emptyResult = false;
+        }         
+    }
+    if (emptyResult)
+    {cout << "No employees matching criteria found" << endl;}
 }
 
 void searchByHourlyPay()
 {
-     float rate;
-     bool emptyResult = true;
-     int extraCounter = 1;
-     cout << "Please enter employee hourly pay rate: ";
-     cin >> rate;
-     cout << "Employees found:" << endl;
-     for (int i = 0; i < EMPLOYEESNUMBER; i++)
-     {
-         if (EmplArr[i].PayRate == rate)
-         {
-              outputResult(extraCounter, i);
-              emptyResult = false;
-         }         
-     }
-     if (emptyResult)
-     {cout << "No employees matching criteria found" << endl;}
+    float rate;
+    bool emptyResult = true;
+    int extraCounter = 1;
+    cout << "Please enter employee hourly pay rate: ";
+    cin >> rate;
+    cout << "Employees found:" << endl;
+    for (int i = 0; i < EMPLOYEESNUMBER; i++)
+    {
+        if (EmplArr[i].PayRate == rate)
+        {
+            outputResult(extraCounter, i);
+            emptyResult = false;
+        }         
+    }
+    if (emptyResult)
+    {cout << "No employees matching criteria found" << endl;}
 }
 
 void outputResult(int par1, int par2)
 {
-     cout << par1 << ") " << EmplArr[par2].ID << " - " <<
-              EmplArr[par2].FirstName << " " << EmplArr[par2].LastName << " - " <<
-              EmplArr[par2].PayRate << "$/hour" << endl;
+//function takes:
+//par1 - number of search result for numbering several search results
+//par2 - number of employee in array
+//Function formats the string and outputs it
+    cout << par1 << ") " << EmplArr[par2].ID << " - " <<
+        EmplArr[par2].FirstName << " " << EmplArr[par2].LastName << " - " <<
+        EmplArr[par2].PayRate << "$/hour" << endl;
 }
