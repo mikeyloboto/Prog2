77e06b6cf6db1751bd451f3096af4d8bf071780c
Dmitrii Kondratev <dimonloboto@gmail.com>
2013-01-29 20:14:48 -0600

    Update Cpp/Assignment 2/Prog2_2_Unreadable.cpp

--- Cpp/Assignment 2/Prog2_2_Unreadable.cpp
+++ Cpp/Assignment 2/Prog2_2_Unreadable.cpp
@@ -14,7 +14,6 @@ cin.getline(b,6);
 c=false;
 for(d=0;d<5;d++)!isdigit(b[d])?c=true:cout<<"";
 c?cout<<"Please, check the correctness of input data and try again"<<endl:cout<<"";}
-int d;
 cout<<endl<<"Enter check digit: ";
 cin>>d;
 10-((((((a[1]*1)+(a[3]*1))+((b[0]*1)+(b[2]*1)+(b[4]*1)))*3)+((a[0]*1)+(a[2]*1)+(a[4]*1))+((b[1]*1)+(b[3]*1)))%10==d)?cout<<endl<<endl<<"Code is valid"<<endl:cout<<endl<<endl<<"Code is invalid"<<endl;
